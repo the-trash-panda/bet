7216d8f86cf695460083a4b76e9d0a26e4d01408
Catherine Chiu <chiuycatherine@gmail.com>
2022-01-27 13:56:42 -0800

    added configExample.js

---
+++ configExample.js
@@ -0,0 +1,9 @@
+module.exports = {
+  userAgent: 'userAgent here',
+  clientId: 'Reddit clientId here',
+  clientSecret: 'Reddit clientSecret here',
+  username: 'Reddit username here',
+  password: 'Reddit password here',
+  alphavantage: 'AlphaVantage API Key here',
+  newsapi: 'News API Key here',
+}
